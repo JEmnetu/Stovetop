85595822b4194b3e5bacc85ab80e8ebfa60a47a4
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-01-30 20:48:50 -0500

    created db folder

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE recipes_db;
+
+USE recipes_db;
+
+CREATE TABLE recipes (
+    id INT AUTO_INCREMENT NOT NULL,
+    recipe_name VARCHAR(30),
+    cook_time TIME
+    -- just some sample categories, still need to be fully fleshed out--
+)
