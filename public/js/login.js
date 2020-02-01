79143d30d17ca1910621c12c57b0d3861831d9ea
JHabte <j.habtema@gmail.com>
2020-02-01 12:01:15 -0500

    working on login

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {
-  // Getting references to our form and inputs
-  var loginForm = $("form.login");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+    // Getting references to our form and inputs
+    var loginForm = $("form.login");
+    var emailInput = $("input#email-input");
+    var usernameInput = $("input#Username-input");
+    var passwordInput = $("input#password-input");
 
-  // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
-    event.preventDefault();
-    var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
+    // When the form is submitted, we validate there's an email and password entered
+    loginForm.on("submit", function(event) {
+        event.preventDefault();
+        var userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim(),
+            username: usernameInput.val().trim()
+        };
 
-    if (!userData.email || !userData.password) {
-      return;
+        if (!userData.username || !userData.password) {
+            return;
+        }
+
+        // If we have an email and password we run the loginUser function and clear the form
+        loginUser(userData.username, userData.password);
+        usernameInput.val("");
+        passwordInput.val("");
+        // emailInput.val("");
+    });
+
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(username, password, ) {
+        $.post("/api/login", {
+
+                username: username,
+                password: password
+            })
+            .then(function() {
+                window.location.replace("/members");
+                // If there's an error, log the error
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
     }
-
-    // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
-  });
-
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password) {
-    $.post("/api/login", {
-      email: email,
-      password: password
-    })
-      .then(function() {
-        window.location.replace("/members");
-        // If there's an error, log the error
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  }
 });
