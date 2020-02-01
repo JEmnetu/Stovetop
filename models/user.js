6273b0a1dfcee2a9e308baad16f6cd0d001e5728
MDBA <artes.mike@gmail.com>
2020-02-01 13:47:42 -0500

    adding sequelize models

--- models/user.js
+++ models/user.js
@@ -16,6 +16,9 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    first_name: {
+      type: DataTypes.STRING
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
