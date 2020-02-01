b57aa9fd72eae30a430593b3b3b7f92d70312c35
JHabte <j.habtema@gmail.com>
2020-02-01 13:38:17 -0500

    added members view. began styling the template views

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,16 +17,18 @@ module.exports = function(app) {
 
     app.get("/login", function(req, res) {
         // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect("/members");
-        }
-        res.sendFile(path.join(__dirname, "../public/login.html"));
+        // if (req.user) {
+        //     res.redirect("/members");
+        // }
+        // res.sendFile(path.join(__dirname, "../public/login.html"));
+        res.render("login")
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/members", isAuthenticated, function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/members.html"));
+    app.get("/members", function(req, res) {
+        // res.sendFile(path.join(__dirname, "../public/members.html"));
+        res.render("members");
     });
 
 };
