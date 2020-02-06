fb6036101a01b7563086e2fa1642434a596504e0
JHabte <j.habtema@gmail.com>
2020-02-06 18:52:50 -0500

    updated routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -49,9 +49,9 @@ module.exports = function(app) {
 
     app.get("/login", function(req, res) {
         // If the user already has an account send them to the members page
-        // if (req.user) {
-        //     res.redirect("/members");
-        // }
+        if (req.user) {
+            res.redirect("/home.html");
+        }
         res.sendFile(path.join(__dirname, "../public/login.html"));
         // res.render("login")
     });
