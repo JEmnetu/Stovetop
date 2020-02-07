2a38ce65f933913df127c60de0f9465b423ddece
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-02-07 16:26:30 -0500

    fixed config issue and search

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 module.exports = {
     "development": {
-        "username": process.env.USERNAME || "root",
+        "username": process.env.DBUSER || "root",
         "password": process.env.PASSWORD || "yourRootPassword",
         "database": process.env.DATABASE || "stovetop_db",
         "host": process.env.HOST || "127.0.0.1",
         "dialect": "mysql"
     },
     "test": {
-        "username": process.env.USERNAME || "root",
+        "username": process.env.DBUSER || "root",
         "password": process.env.PASSWORD || "yourRootPassword",
         "database": process.env.DATABASE || "stovetop_db",
         "host": process.env.HOST || "127.0.0.1",
         "dialect": "mysql"
     },
     "production": {
-        "username": process.env.USERNAME || "root",
+        "username": process.env.DBUSER || "root",
         "password": process.env.PASSWORD || "yourRootPassword",
         "database": process.env.DATABASE || "stovetop_db",
         "host": process.env.HOST || "127.0.0.1",
