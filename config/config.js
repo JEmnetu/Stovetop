cca73d19a3c64cb1bdca3e6613025c4c9fa2ee0f
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-02-07 16:01:35 -0500

    trying to resolve config error

--- config/config.js
+++ config/config.js
@@ -2,21 +2,21 @@ require("dotenv").config();
 module.exports = {
     "development": {
         "username": process.env.USERNAME || "root",
-        "password": process.env.PASSWORD || null,
+        "password": process.env.PASSWORD || "yourRootPassword",
         "database": process.env.DATABASE || "stovetop_db",
         "host": process.env.HOST || "127.0.0.1",
         "dialect": "mysql"
     },
     "test": {
         "username": process.env.USERNAME || "root",
-        "password": process.env.PASSWORD || null,
+        "password": process.env.PASSWORD || "yourRootPassword",
         "database": process.env.DATABASE || "stovetop_db",
         "host": process.env.HOST || "127.0.0.1",
         "dialect": "mysql"
     },
     "production": {
         "username": process.env.USERNAME || "root",
-        "password": process.env.PASSWORD || null,
+        "password": process.env.PASSWORD || "yourRootPassword",
         "database": process.env.DATABASE || "stovetop_db",
         "host": process.env.HOST || "127.0.0.1",
         "dialect": "mysql",
