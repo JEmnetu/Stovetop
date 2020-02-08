46736e67040b710980e84728ec66695c26c73042
MDBA <artes.mike@gmail.com>
2020-02-07 19:46:46 -0500

    added on click for flames

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,4 +1,4 @@
-$("#add-recipe-btn").on("click", function() {
+$("#add-recipe-btn").on("click", function () {
     var public = 1;
     if ($("#private-recipe").val() === "private") {
         public = 0;
@@ -17,19 +17,40 @@ $("#add-recipe-btn").on("click", function() {
         url: "api/recipes",
         method: "POST",
         data: newRecipe
-    }).then(function() {
+    }).then(function () {
         location.href = "/mystovetop";
     });
 });
 
 //search button
 $("#search-btn").on("click", function() {
+    let searchQuery = $("#search-query").val();
+    location.href = "/results?keywords=" + encodeURIComponent(searchQuery);
+    
+});
+//flames button
+$(".flames-btn").on("click", function (event) {
+    // need to get recipe id from the buttton
+    let recipeId = event.target.attr("data-recipe-id");
+    //data-recipe-id="{{ id }}" (handlebars)
+
+    // need to know if recipe is already liked
+    let liked = event.target.attr("data-recipe-liked")
+    //data-recipe-liked="false" (handlebars)
+
     $.ajax({
-        url: "/api/food_data/search",
-        method: "POST",
-        data: 'Results'
-    }).then(function(res) {
-        location.href = "/results";
+        url: "/api/recipes/" + recipeId,
+        method: "PUT",
+        data: {
+            flame: liked ? true : false// TT/F depending on whether already flamed
+        }
+    }).then(function (res) {
+        // toggle the liked status from t=>f or f=>t
+        if (liked === "false") {
+            liked = true
+        } else {
+            liked = false
+        }
         console.log(res);
     });
 
